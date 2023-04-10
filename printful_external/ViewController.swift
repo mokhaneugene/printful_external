e67b4eacfc734fe7e9a2f692c1fa748b0cfa58fc
mokhaneugene <mokhaneugene@gmail.com>
2023-04-10 11:30:55 +0300

    First commit

---
+++ printful_external/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  printful_external
+//
+//  Created by Eugene Mokhan on 10/04/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
